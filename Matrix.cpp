710b4150234100bb9640fdd2ebabfa7068f156be
Josh Keeler <keelerjr12@gmail.com>
2022-09-24 21:25:11 -0500

    Project towards matrix.cpp + tests -JK

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,6 +1,7 @@
 // Project UID af1f95f547e44c8ea88730dfb185559d
 
 #include <cassert>
+#include <limits>
 #include "Matrix.h"
 
 // REQUIRES: mat points to a Matrix
@@ -10,7 +11,12 @@
 // EFFECTS:  Initializes *mat as a Matrix with the given width and height.
 // NOTE:     Do NOT use new or delete here.
 void Matrix_init(Matrix* mat, int width, int height) {
-  assert(false); // TODO Replace with your implementation!
+    assert(0 < width && width <= MAX_MATRIX_WIDTH);
+    assert(0 < height && height <= MAX_MATRIX_HEIGHT);
+
+    Matrix_fill(mat, 0);
+    mat->width = width;
+    mat->height = height;
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -28,39 +34,52 @@ void Matrix_print(const Matrix* mat, std::ostream& os) {
 // REQUIRES: mat points to an valid Matrix
 // EFFECTS:  Returns the width of the Matrix.
 int Matrix_width(const Matrix* mat) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+
+    return mat->width;
 }
 
 // REQUIRES: mat points to a valid Matrix
 // EFFECTS:  Returns the height of the Matrix.
 int Matrix_height(const Matrix* mat) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+
+    return mat->height;
 }
 
 // REQUIRES: mat points to a valid Matrix
 //           ptr points to an element in the Matrix
 // EFFECTS:  Returns the row of the element pointed to by ptr.
 int Matrix_row(const Matrix* mat, const int* ptr) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+    assert(ptr >= mat->data && (mat->data + (mat->width * mat->height) - 1));
+
+    return (ptr - mat->data) / mat->width;
 }
 
 // REQUIRES: mat points to a valid Matrix
 //           ptr point to an element in the Matrix
 // EFFECTS:  Returns the column of the element pointed to by ptr.
 int Matrix_column(const Matrix* mat, const int* ptr) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+    assert(ptr >= mat->data && (mat->data + (mat->width * mat->height) - 1));
+
+    return (ptr - mat->data) % mat->width;
 }
 
 // REQUIRES: mat points to a valid Matrix
 //           0 <= row && row < Matrix_height(mat)
 //           0 <= column && column < Matrix_width(mat)
-//
 // MODIFIES: (The returned pointer may be used to modify an
 //            element in the Matrix.)
 // EFFECTS:  Returns a pointer to the element in the Matrix
 //           at the given row and column.
 int* Matrix_at(Matrix* mat, int row, int column) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+    assert(0 <= row && row <= Matrix_height(mat));
+    assert(0 <= column && column <= Matrix_width(mat));
+
+    return &mat->data[row * Matrix_width(mat) + column];
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -70,14 +89,24 @@ int* Matrix_at(Matrix* mat, int row, int column) {
 // EFFECTS:  Returns a pointer-to-const to the element in
 //           the Matrix at the given row and column.
 const int* Matrix_at(const Matrix* mat, int row, int column) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+    assert(0 <= row && row <= Matrix_height(mat));
+    assert(0 <= column && column <= Matrix_width(mat));
+
+    return &mat->data[row * Matrix_width(mat) + column];
 }
 
 // REQUIRES: mat points to a valid Matrix
 // MODIFIES: *mat
 // EFFECTS:  Sets each element of the Matrix to the given value.
 void Matrix_fill(Matrix* mat, int value) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+
+    for (auto row = 0; row < mat->height; ++row) {
+        for (auto col = 0; col < mat->width; ++col) {
+            mat->data[row * mat->width + col] = value;
+        }
+    }
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -86,13 +115,29 @@ void Matrix_fill(Matrix* mat, int value) {
 //           the given value. These are all elements in the first/last
 //           row or the first/last column.
 void Matrix_fill_border(Matrix* mat, int value) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+
+    assert(false); // TODO Replace with your implementation!
 }
 
 // REQUIRES: mat points to a valid Matrix
 // EFFECTS:  Returns the value of the maximum element in the Matrix
 int Matrix_max(const Matrix* mat) {
-  assert(false); // TODO Replace with your implementation!
+    assert(mat);
+
+    auto max = std::numeric_limits<int>::min();
+
+    for (auto row = 0; row < Matrix_height(mat); ++row) {
+        for (auto col = 0; col < Matrix_width(mat); ++col) {
+            auto curr_val = *Matrix_at(mat, row, col);
+            
+            if (max <= curr_val) {
+                    max = curr_val;
+            }
+        }
+    }
+
+    return max;
 }
 
 // REQUIRES: mat points to a valid Matrix
--- Matrix_tests.cpp
+++ Matrix_tests.cpp
@@ -6,11 +6,6 @@
 
 using namespace std;
 
-
-// Here's a free test for you! Model yours after this one.
-// Test functions have no interface and thus no RMEs, but
-// add a comment like the one here to say what it is testing.
-// -----
 // Fills a 3x5 Matrix with a value and checks
 // that Matrix_at returns that value for each element.
 TEST(test_fill_basic) {
@@ -31,14 +26,102 @@ TEST(test_fill_basic) {
   delete mat; // delete the Matrix
 }
 
-// ADD YOUR TESTS HERE
-// You are encouraged to use any functions from Matrix_test_helpers.h as needed.
+// Initialize matrix and query the given width
+TEST(init_matrix_query_width) {
+  Matrix *mat = new Matrix; 
+  const int width = 3;
+  const int height = 5;
 
+  Matrix_init(mat, width, height);
 
-// NOTE: The unit test framework tutorial in Lab 2 originally
-// had a semicolon after TEST_MAIN(). Although including and
-// excluding the semicolon are both correct according to the c++
-// standard, the version of g++ we are using reports a warning
-// when there is a semicolon. Therefore do NOT add a semicolon
-// after TEST_MAIN()
+  ASSERT_EQUAL(Matrix_width(mat), width);
+  delete mat;
+}
+
+// Initialize matrix and query the given height
+TEST(init_matrix_query_height) {
+  Matrix *mat = new Matrix; 
+  const int width = 3;
+  const int height = 5;
+
+  Matrix_init(mat, width, height);
+
+  ASSERT_EQUAL(Matrix_height(mat), height);
+  delete mat;
+}
+
+// Return first row of specified element
+TEST(filled_matrix_element_first_row_return_first_row) {
+    Matrix *mat = new Matrix; 
+    const int width = 3;
+    const int height = 5;
+  
+    Matrix_init(mat, width, height);
+    Matrix_fill(mat, 5);
+    auto el = Matrix_at(mat, 0, 0);
+
+    ASSERT_EQUAL(Matrix_row(mat, el), 0);
+    delete mat;
+}
+
+// Return last row of specified element
+TEST(filled_matrix_element_last_row_return_last_row) {
+    Matrix *mat = new Matrix; 
+    const int width = 3;
+    const int height = 5;
+  
+    Matrix_init(mat, width, height);
+    Matrix_fill(mat, 5);
+    auto el = Matrix_at(mat, height-1, 0);
+
+    ASSERT_EQUAL(Matrix_row(mat, el), height-1);
+    delete mat;
+}
+
+// Return first column of specified element
+TEST(filled_matrix_element_first_column_return_first_column) {
+    Matrix *mat = new Matrix; 
+    const int width = 3;
+    const int height = 5;
+  
+    Matrix_init(mat, width, height);
+    Matrix_fill(mat, 5);
+    auto el = Matrix_at(mat, 0, 0);
+
+    ASSERT_EQUAL(Matrix_column(mat, el), 0);
+    delete mat;
+}
+
+// Return last column of specified element
+TEST(filled_matrix_element_last_column_return_last_column) {
+    Matrix *mat = new Matrix; 
+    const int width = 3;
+    const int height = 5;
+  
+    Matrix_init(mat, width, height);
+    Matrix_fill(mat, 5);
+    auto el = Matrix_at(mat, height-1, width-1);
+
+    ASSERT_EQUAL(Matrix_column(mat, el), width-1);
+    delete mat;
+}
+
+// Return max value
+TEST(filled_matrix_return_max_element) {
+    Matrix *mat = new Matrix; 
+    const int width = 3;
+    const int height = 5;
+  
+    Matrix_init(mat, width, height);
+    Matrix_fill(mat, 5);
+    *Matrix_at(mat, height/2, width/2) = 10;
+    *Matrix_at(mat, 0, width-1) = 40;
+    *Matrix_at(mat, height-1, 0) = 30;
+    *Matrix_at(mat, height-1, width-1) = 20;
+
+    
+    ASSERT_EQUAL(Matrix_max(mat), 40);
+    delete mat;
+}
+
 TEST_MAIN() // Do NOT put a semicolon here
